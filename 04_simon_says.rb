aa205918e93fea55bcf2522af5b90b758007090c
Andrygaby <angeg7360@gmail.com>
2019-07-16 17:12:59 +0300

    suite teste

---
+++ 04_simon_says.rb
@@ -0,0 +1,58 @@
+def Simon says
+	def echo (hello)
+		return" #{hello}"
+		def echo (bye)
+			return " #{bye}"
+		end
+	end
+	return Simon says
+
+	def shout (hello)
+		return "#{HELLO}"
+		def shout (multiple_words)
+			return "#{HELLO WORLD}"
+		end 
+	end
+
+	def repeat
+		return "#{"hello"*2}"
+	end
+	def repeat (hello,n)
+		return "#{"hello" * 3}"
+	end
+
+	def start_of_word (hello)
+		word='hello'
+		return word=[0]
+		word='Bob'
+		return word=[0,1]
+		s = "abcdefg"
+		return s=[0]
+		return s=[0,1]
+		return s=[0,1,2]
+	end
+
+	def first_word
+		s=["hello","world"]
+		return s=[0]
+		s1=["oh", "dear"]
+		return s1=[0]
+	end
+
+	def titleize
+		w="jaws"
+		return w.capitalize
+		def every
+			w1="david","copperfield"
+		return w1=[0,1].capitalize
+			def little
+				w2="war and peace"
+			return w2.capitalize
+				def title
+					w3="the bridge over the river kwai"
+					return w3.capitalize
+				end
+			end
+		end
+	end
+end
